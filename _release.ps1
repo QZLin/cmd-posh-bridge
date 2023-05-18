d4f02d54dbdfa0071436e9578843977f2490e2e2
QZLin <qzlin01@163.com>
2023-05-18 22:17:05 +0800

    0.6

--- _release.ps1
+++ _release.ps1
@@ -1,9 +1,15 @@
 Push-Location $PSScriptRoot
 
-Remove-Item release -Recurse
+Remove-Item release -Recurse -ErrorAction Ignore
 New-Item release -ItemType Directory
 Copy-Item cmd-posh-bridge.* release
 Copy-Item LICENSE release
 Copy-Item README.md release
 
 Pop-Location
+
+<# https://github.com/PowerShell/PowerShellGetv2/issues/606#issuecomment-906652159
+Install-Module PowerShellGet -AllowPrerelease -Repository PSGallery -Force
+Import-Module PowerShellGet
+Publish-PSResource -path release -apikey ***
+#>
